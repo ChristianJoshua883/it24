cab4d5032e23a7fb57e48ce5d0e978c423d08efe
niga <20231874@nbsc.edu.ph>
2024-10-03 01:24:19 +0800

    hello

---
+++ app.js
@@ -0,0 +1,19 @@
+//A. Data types
+// Numbers:
+let length = 16;
+let weight = 7.5;
+// Strings:
+let color = "Yellow";
+let lastName = "Tulod";
+// Booleans
+let x = true;
+let y = false;
+// Object:
+const person = {firstName:"Christian", lastName:"Tulod"};
+// Array object:
+const motors = ["YAMAHA", "HONDA", "RUSI"];
+// Date object:
+const date = new Date("2022-03-25");
+
+// B.
+
