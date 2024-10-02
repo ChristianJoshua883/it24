fb319eafa551e212d8c238be75046eb159455340
niga <20231874@nbsc.edu.ph>
2024-10-03 01:33:40 +0800

    added and modified the data types and added Arithmetic and logical operators

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ let weight = 7.5;
 let color = "Yellow";
 let lastName = "Tulod";
 // Booleans
-let x = true;
-let y = false;
+let incorrect = true;
+let correct = false;
 // Object:
 const person = {firstName:"Christian", lastName:"Tulod"};
 // Array object:
@@ -15,5 +15,21 @@ const motors = ["YAMAHA", "HONDA", "RUSI"];
 // Date object:
 const date = new Date("2022-03-25");
 
-// B.
+// B. Arithmetic and logical operators
+let x = 5;
+let y = 10;
+
+let sum = x + y;
+let diff = x - y;
+let prod = x * y;
+let qou = x / y;
+let mod = x % y;
+
+console.log("the sum is " + sum );
+console.log("the difference is " + diff );
+console.log("the product is " + prod );
+console.log("the qoutient is " + qou );
+console.log("the modulo is " + mod );
+
+
 
