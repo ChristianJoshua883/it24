6846adebb835a42854a0443b2a16e343716a445c
niga <20231874@nbsc.edu.ph>
2024-10-06 11:39:58 +0800

    added added class DataLogger

--- BTN-Logger.js
+++ BTN-Logger.js
@@ -1,11 +1,15 @@
-//Event Function 1
-function updateDisplay() {
-    var textbox = document.getElementById('textbox');
-    var displayText = document.getElementById('displayText');
-    displayText.textContent = textbox.value;
-}
 
-function showAlert() {
-    var textbox = document.getElementById('textbox');
-    alert("Hello " + textbox.value);
+
+
+class DataLogger{
+    constructor(buttonId, cardContainerId, clearButtonId, logCountId){
+        this.logButton = document.getElementById(buttonId);
+        this.clearButton = document.getElementById(clearButtonId);
+        this.idContainer = document.getElementById(cardContainerId);
+        this.logCountElement = document.getElementById(logCountId);
+        this.loggedData = [];     
+
+        this.logButton.addEventListener('click', () => this.logData());
+        this.clearButton.addEventListener('click', () => this.clearLogs());
+    }
 }
