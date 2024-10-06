89c71c292e2dd4db8103adc5b99ea5241b400e39
niga <20231874@nbsc.edu.ph>
2024-10-06 21:16:34 +0800

    added getElemetBYId

--- attendance/L07.js
+++ attendance/L07.js
@@ -4,6 +4,30 @@ class LeafletMap {
     constructor(containerId, center, zoom) {
         this.map = L.map(containerId).setView(center, zoom);
         this.initTileLayer();
+
+
+        this.attendanceCountSC = 0;
+        this.attendanceCountBA = 0;
+        this.attendanceCountLab = 0;
+
+        this.markerCounts = {};
+        this.markers = [];
+
+        this.loggedData = []; 
+
+        this.SC = document.getElementById('SC');
+        this.btn1 = document.getElementById('btn1');
+        this.btn2 = document.getElementById('btn2');
+        this.btnclear = document.getElementById('btnclear');
+        this.logCountElement = document.getElementById('logCountSC');
+        this.logCount1Element = document.getElementById('logCountLibrary');
+        this.logCount2Element = document.getElementById('logCountGym');
+        this.logContainer = document.getElementById('logContainer');
+
+        this.btn.addEventListener('click', () => this.dataSc());
+        this.btn1.addEventListener('click', () => this.dataLab());
+        this.btn2.addEventListener('click', () => this.dataBa());
+        this.btnclear.addEventListener('click', () => this.clearLogs());
     }
 
 
@@ -34,4 +58,17 @@ class LeafletMap {
 
 const myMap = new LeafletMap('map', [8.360004, 124.868419], 18);
 
+
+
+
+
+
 myMap.loadMarkersFromJson('L07.json');
+
+
+
+
+
+
+
+
