cfbd954753f9982455b7f202ddc56873f23b29e3
niga <20231874@nbsc.edu.ph>
2024-10-17 10:15:32 +0800

    added async fetchweather method

--- applet-5/applet-5.js
+++ applet-5/applet-5.js
@@ -38,3 +38,24 @@ class openweather {
         }       
     }
 
+    
+    async fetchWeather() {
+        const city = this.cityInput.value;
+        this.apiKey = this.APIKeyInput.value.trim(); 
+
+        if (!this.apiKey) {
+            alert('Please enter your API key.');
+            return;
+        }
+
+        if (city) {
+            const data = await this.getWeatherData(city);
+            if (data) {
+                this.displayWeather(data);
+            } else {
+                alert('City not found. Please try again.');
+            }
+        } else {
+            alert('Please enter a city name.');
+        }
+    }
